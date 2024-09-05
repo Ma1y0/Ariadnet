905c78d773952f3407b5500c1e387f6311a27261
Matyáš Bár <matyas.barr@gmail.com>
2024-09-05 10:12:32 +0200

    test(echo): Add end-to-end test

--- echo/tests/end_to_end.rs
+++ echo/tests/end_to_end.rs
@@ -1,11 +1,10 @@
 use echo::kv::Store;
+use echo::packet::Packet;
 use std::net::SocketAddr;
-use std::sync::Arc;
 use tokio::net::UdpSocket;
 use tokio::sync::oneshot;
-use tokio::time::{sleep, Duration};
 
-async fn spawn_server(store: Arc<Store>) -> (SocketAddr, oneshot::Sender<()>) {
+async fn spawn_server(store: Store) -> (SocketAddr, oneshot::Sender<()>) {
     let socket = UdpSocket::bind("127.0.0.1:0")
         .await
         .expect("Failed to bind server socket");
@@ -15,7 +14,7 @@ async fn spawn_server(store: Arc<Store>) -> (SocketAddr, oneshot::Sender<()>) {
 
     tokio::spawn(async move {
         tokio::select! {
-            _ = echo::server::serve(socket, store.as_ref()) => {
+            _ = echo::server::serve(socket, store) => {
                 println!("Server stopped");
             }
             _ = shutdown_rx => {
@@ -29,11 +28,8 @@ async fn spawn_server(store: Arc<Store>) -> (SocketAddr, oneshot::Sender<()>) {
 
 #[tokio::test]
 async fn test_resolve_query_not_found() {
-    let store = Arc::new(Store::in_memory());
-    let (addr, shutdown_tx) = spawn_server(Arc::clone(&store)).await;
-
-    // Give the server some time to start up
-    sleep(Duration::from_millis(100)).await;
+    let store = Store::in_memory();
+    let (addr, shutdown_tx) = spawn_server(store).await;
 
     let socket = UdpSocket::bind("127.0.0.0:0")
         .await
@@ -69,10 +65,56 @@ async fn test_resolve_query_not_found() {
         "",
     );
 
+    // Tests
     assert_eq!(expected, ans);
 
     // Shutdown the server
     shutdown_tx
         .send(())
         .expect("Failed to send shutdown signal");
+}
+
+#[tokio::test]
+async fn test_resolve_query() {
+    let mut store = Store::in_memory();
+    store.insert("a", "a");
+    let (addr, shutdown_tx) = spawn_server(store).await;
+
+    // Create UDP client
+    let socket = UdpSocket::bind("127.0.0.1:0")
+        .await
+        .expect("Failed to bind the UDP socket");
+
+    // Packet
+    let packet: Vec<u8> = Packet::new(
+        1,
+        echo::packet::Method::Query,
+        echo::packet::Error::None,
+        "a",
+    )
+    .into();
+
+    // Send packet
+    socket
+        .send_to(&packet, addr)
+        .await
+        .expect("Failed to send query");
+
+    // Read response
+    let mut buf = [0u8; 1024];
+    let n = socket
+        .recv(&mut buf)
+        .await
+        .expect("Failed to receive answer");
+
+    // Parse the answer
+    let answer = Packet::try_from(&buf[..n]).expect("Failed to parse the answer");
+
+    // Tests
+    assert_eq!("a", answer.body);
+
+    // Shutdown the server
+    shutdown_tx
+        .send(())
+        .expect("Faile to send shutdown message to the server");
 }
